b2162fbb1d9980acf3eb5ea642faaf27710db9bb
Mahmud Hasan <mah5472651@gmail.com>
2026-08-12 13:26:15 +0600

    docs: add image to members page

--- src/pages/resources/workspace/people-roles/MembersPage.tsx
+++ src/pages/resources/workspace/people-roles/MembersPage.tsx
@@ -1,4 +1,4 @@
-﻿import DocsLayout from '../../DocsLayout';
+import DocsLayout from '../../DocsLayout';
 
 export default function MembersPage() {
   return (
@@ -14,7 +14,10 @@ export default function MembersPage() {
       </div>
 
       <h1 className="docs__title" id="members">Members</h1>
-      <p className="docs__text">Content coming soon...</p>
+      
+      <div className="docs__image-container" style={{ marginTop: '32px' }}>
+        <img src="/assets/Members/1.png" alt="Members" className="docs__image" />
+      </div>
     </DocsLayout>
   );
 }
