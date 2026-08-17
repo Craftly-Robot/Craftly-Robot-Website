4cb3bdace5bd4a496ae425217e46f16be3f515b9
Mahmud Hasan <mah5472651@gmail.com>
2026-08-17 14:57:58 +0600

    fix: use min-height for SupportCategoryMenu to prevent layout shift while keeping vertical center alignment

--- src/pages/use-cases/OperationsPage.tsx
+++ src/pages/use-cases/OperationsPage.tsx
@@ -110,7 +110,6 @@ export default function OperationsPage() {
         }
         visual={<SupportCategoryMenu />}
         layout="text-right"
-        alignTop={true}
       />
 
       <UseCaseCTA title="Download Craftly Workspace" />
