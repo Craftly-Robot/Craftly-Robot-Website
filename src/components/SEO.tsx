88b0907b9d75754e2a2fb3073742dc8e59e4bc04
tarekul42 <tarekulrifat205@gmail.com>
2026-09-02 19:10:23 +0600

    chore: rename Telegram-style asset filename to craftly-logo.jpg

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -15,7 +15,7 @@ export function SEO({
   name = "Craftly",
   type = "website",
   url = "https://craftlyrobot.com",
-  image = "https://craftlyrobot.com/assets/craftly_logo/photo_2026-08-12_19-27-43.jpg",
+  image = "https://craftlyrobot.com/assets/craftly_logo/craftly-logo.jpg",
 }: SEOProps) {
   const fullTitle = `${title} | Craftly`;
 
