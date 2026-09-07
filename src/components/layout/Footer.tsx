8e73ffabaadac2f783a01d18eb41aae0764d5eab
tarekul42 <tarekulrifat205@gmail.com>
2026-09-07 07:00:40 +0600

    feat: redesign Footer to be more professional
    - Dark background (#111111)
    - Space Grotesk font
    - Clean layout: brand + tagline, 4-column links, copyright
    - Added Connect column (GitHub, Support)
    - Subtle hover effects
    - Mobile responsive

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -5,12 +5,23 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer className="footer" role="contentinfo">
-      <div className="footer__top">
-        <div className="footer__top-left">
-          <h2 className="footer__heading">Experience liftoff</h2>
+      <div className="footer__inner">
+        {/* Top: Brand + Tagline */}
+        <div className="footer__brand">
+          <Link href="/" className="footer__logo">
+            <ImageWithFallback
+              className="footer__wordmark"
+              src="/assets/brand/craftly-wordmark.svg"
+              alt="Craftly"
+            />
+          </Link>
+          <p className="footer__tagline">
+            The operating system for intelligent organizations.
+          </p>
         </div>
 
-        <div className="footer__top-right">
+        {/* Middle: Links */}
+        <div className="footer__links">
           <div className="footer__column">
             <h3 className="footer__column-title">Product</h3>
             <Link href="/products/workspace" className="footer__link">
@@ -35,8 +46,8 @@ export default function Footer() {
             <Link href="/resources/releases" className="footer__link">
               Releases
             </Link>
-            <Link href="/use-cases" className="footer__link">
-              Use Cases
+            <Link href="/resources/documentation/faq" className="footer__link">
+              FAQ
             </Link>
           </div>
 
@@ -48,31 +59,36 @@ export default function Footer() {
             <Link href="/about" className="footer__link">
               About
             </Link>
+            <Link href="/use-cases" className="footer__link">
+              Use Cases
+            </Link>
             <Link href="/get-involved" className="footer__link">
               Get Involved
+            </Link>
+          </div>
+
+          <div className="footer__column">
+            <h3 className="footer__column-title">Connect</h3>
+            <a
+              href="https://github.com/Craftly-Robot"
+              className="footer__link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+            <Link href="/resources/support" className="footer__link">
+              Support
             </Link>
           </div>
         </div>
       </div>
 
+      {/* Bottom: Copyright */}
       <div className="footer__bottom">
-        <div className="footer__bottom-left">
-          <Link href="/" className="footer__bottom-logo">
-            <ImageWithFallback
-              className="footer__wordmark footer__wordmark--small"
-              src="/assets/brand/craftly-wordmark.svg"
-              alt="Craftly"
-            />
-          </Link>
-        </div>
-        <div className="footer__bottom-right">
-          <Link href="/resources/support" className="footer__bottom-link">
-            Support
-          </Link>
-          <Link href="/resources/documentation" className="footer__bottom-link">
-            Docs
-          </Link>
-        </div>
+        <p className="footer__copyright">
+          &copy; {new Date().getFullYear()} Craftly. All rights reserved.
+        </p>
       </div>
     </footer>
   );
